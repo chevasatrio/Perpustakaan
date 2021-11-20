d21ce91f3c38dde27fa6189817eebcba999cdabf
Cheva Satrio <73184796+chevasatrio@users.noreply.github.com>
2021-11-20 21:34:39 +0700

    Add files via upload

---
+++ Perpustakaan2/home.php
@@ -0,0 +1,14 @@
+<?php 
+    include "header.php";
+?>
+<center>
+<h1>SELAMAT DATANG
+    <br>
+    <br>
+    DI WEBSITE PERPUSTAKAAN ONLINE!</h1>
+    <br>
+    <img src="hey.png">
+</center>
+<?php
+    include "footer.php";
+?>
---
+++ Perpustakaan2/index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+
+
+
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+  <!-- Brand/logo -->
+  <a class="navbar-brand" href="#">
+    <img src="bo.jpg" alt="logo" style="width:40px;">
+  </a>
+  
+  <!-- Links -->
+  <ul class="navbar-nav">
+    <li class="nav-item active">
+      <a class="nav-link" href="index.php">Home</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="Tampil_siswa.php">Data Siswa</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="tampil_kelas.php">Data Kelas</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link disabled" href=""></a>
+    </li>
+  </ul>
+</nav>
+
+<div class="jumbotron text-center" style="margin-top:20px"style="margin-bottom:20px">
+    <h1>Welcome Ngab</h1>
+    <p>Hi Channel, Welcome to my guys </p> 
+    </div>
+
+    
+
+
+  <div class="col-sm-4">
+      <h3>Ini Codingan saya</h3>
+      <p>Jalan Jalan ke pasar belimbing <br>
+    membeli kerupuk</p>
+      <p>hmmmm </p>
+    </div>
+
+
+</body>
+</html>
